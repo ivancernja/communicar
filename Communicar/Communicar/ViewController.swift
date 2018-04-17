30d0970c87a0ca51484bb48b0d313ca0cab58a1d
Filip Varda <filip.varda@q-software.com>
2018-04-17 23:39:40 +0200

    -init commit

---
+++ Communicar/Communicar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Communicar
+//
+//  Created by Filip Varda on 17/04/2018.
+//  Copyright © 2018 Communicar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
